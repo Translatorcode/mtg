c61a4c090594993c86b77671f1aa68c1b1af5a3a
Canny Wu <canny.wu90@gmail.com>
2023-04-28 16:39:12 -0400

    Router Setup and HeroSection

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='navbar navbar-expand-lg bg-body-tertiary col-primary-custom'>
+        <div className='container-fluid '>
+          <a className='navbar-brand text-white' href='#'>
+            MortgageLytics
+          </a>
+          <button
+            className='navbar-toggler navbar-dark'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon navbar-dark'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav ms-auto mb-2 mb-lg-0 '>
+              <li className='nav-item'>
+                <a className='nav-link text-custom-one ' href='#'>
+                  About
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link text-custom-one' aria-current='page' href='#'>
+                  Faq
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link text-custom-one ' href='#'>
+                  Calculator
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/screens/Homepage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import HeroImage from '../otherassets/HeroImage.png';
+
+// Bootstrap Note
+// In Bootstrap, when you define a class with a breakpoint-specific suffix (such as sm, md, lg, xl, or xxl), the styles defined in that class will only apply to the element when the screen width is equal to or greater than the breakpoint size.
+
+// If you don't specify a breakpoint-specific suffix, the styles defined in that class will apply to the element at all screen sizes, including those below the breakpoint size
+
+const Homepage = () => {
+  return (
+    <section className='hero overflow-hidden'>
+      <div className='container col-bg-light-blue mt-5 p-5'>
+        <div className='row no-gutters align-items-center justify-content-between'>
+          <div className='col-md-6 order-md-1 order-last'>
+            <div className='hero-content text-center text-md-start  mt-md-0 mt-5'>
+              <h1 className='display-1 lh-1 mb-3 text-secondary-custom fw-bold '>
+                Mortgage
+                <span className='text-custom-two '> payment calculator.</span>
+              </h1>
+              <p className='hero-subheading fw-semibold mb-4'>Calculate your way to financial freedom.</p>
+            </div>
+          </div>
+          <div className='col-md-6 d-lg-block order-md-2 order-first'>
+            <div className='hero-image-content d-flex align-items-center'>
+              <img src={HeroImage} class='img-fluid' alt='house' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Homepage;
